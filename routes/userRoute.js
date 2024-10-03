bd91d0f4726008f0c2a709b2560431bc79d79d08
BrajanS <szczapbrajan33@gmail.com>
2024-10-03 11:39:51 +0200

    1.05

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,27 +1,37 @@
 const express = require('express')
 const userRoute = express()
 const userModel = require('../models/userModel.js')
+const bcrypt = require('bcryptjs')
 
 userRoute.get('/users',async (req,res)=>{
     try {
         const users = await userModel.find()
         res.status(200).json(users)
     } catch (error) {
-        res.status(500).json({
-            message:error.message
-        })
+        console.log("Dans userRoute.get: ",error.message)
+        res.status(500).json({message:error.message})
     }
 })
 
 userRoute.post('/register',async (req,res)=>{
+    const userData = req.body
     try {
-        const userData = req.body
+        const email = userData.email // Email écrit dans req.body
+        const email_Exists = await userModel.findOne({email})
+        if(email_Exists){           // TRUE, si l'email est déjà dans la Base de données
+            return res.status(400).json(`Email "${email}" déjà utilisé`)
+        }
+        // Crypting settings
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(userData.password,salt)
+        // Makes the password in req.body, Hashed aka Crypted
+        userData.password = hash
+        // POST les données écrite dans le req.body
         await userModel.create(userData)
-        res.status(200).json(userData)
+        res.json({message:`L'utilisateur "${userData.username}" à été ajouté avec succes !`})
     } catch (error) {
-        res.status(500).json({
-            message:error.message
-        })
+        console.log("Dans userRoute.post: ",error.message);
+        res.status(500).json({message:error.message})
     }
 })
 
